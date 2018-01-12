9c29a747c5cc29b1bba9a6fac3b7cc33e7443e78
Bartosz Michalik <bartosz.michalik@amartus.com>
2018-01-12 16:48:43 +0100

    fixing problems related to Oxygen tool upgrades
    Change-Id: I6e607071d5518f44377883602df3db718b06dc76
    Signed-off-by: Bartosz Michalik <Bartosz.Michalik@amartus.com>

--- impl/src/main/java/org/opendaylight/unimgr/mef/nrp/impl/AbstractNodeHandler.java
+++ impl/src/main/java/org/opendaylight/unimgr/mef/nrp/impl/AbstractNodeHandler.java
@@ -80,6 +80,7 @@ public class AbstractNodeHandler implements DataTreeChangeListener<Topology> {
                         .map(DataTreeModification::getRootNode)
                         .flatMap(topo -> topo.getModifiedChildren().stream())
                         .flatMap(node -> node.getModifiedChildren().stream())
+                        .filter(this::isNep)
                         .filter(this::checkIfUpdated)
                         .map(nep -> (OwnedNodeEdgePoint) nep.getDataAfter())
                         .collect(Collectors.toList());
@@ -89,6 +90,7 @@ public class AbstractNodeHandler implements DataTreeChangeListener<Topology> {
                 .map(DataTreeModification::getRootNode)
                 .flatMap(topo -> topo.getModifiedChildren().stream())
                 .flatMap(node -> node.getModifiedChildren().stream())
+                .filter(this::isNep)
                 .filter(this::checkIfDeleted)
                 .map(nep -> (OwnedNodeEdgePoint) nep.getDataBefore())
                 .collect(Collectors.toList());
@@ -114,6 +116,10 @@ public class AbstractNodeHandler implements DataTreeChangeListener<Topology> {
                 LOG.warn("Abstract TAPI node upadate failed due to an error", t);
             }
         });
+    }
+
+    private boolean isNep(DataObjectModification dataObjectModificationNep) {
+        return OwnedNodeEdgePoint.class.isAssignableFrom(dataObjectModificationNep.getDataType());
     }
 
     private boolean checkIfDeleted(DataObjectModification dataObjectModificationNep) {
