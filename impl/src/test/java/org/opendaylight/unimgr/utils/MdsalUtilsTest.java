76e06b732e010f8648669c32592fbd5320e2007b
Bartosz Michalik <Bartosz.Michalik@amartus.com>
2018-05-31 12:17:36 +0200

    Removing deprecated model (api module) along with supporting code
    Change-Id: Ia49b05a5ab11bcfad79612d06057fd769e541097
    Signed-off-by: Bartosz Michalik <Bartosz.Michalik@amartus.com>

--- impl/src/test/java/org/opendaylight/unimgr/utils/MdsalUtilsTest.java
+++ impl/src/test/java/org/opendaylight/unimgr/utils/MdsalUtilsTest.java
@@ -8,14 +8,10 @@
 
 package org.opendaylight.unimgr.utils;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.argThat;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Appender;
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.CheckedFuture;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -29,22 +25,18 @@ import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
-import org.opendaylight.unimgr.impl.UnimgrConstants;
-import org.opendaylight.unimgr.mef.nrp.api.EndPoint;
-import org.opendaylight.unimgr.mef.nrp.common.TapiUtils;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.PortDirection;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.Uuid;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.connectivity.rev180307.ConnectivityServiceEndPoint;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.connectivity.rev180307.connectivity.service.end.point.ServiceInterfacePoint;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.*;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.LinkId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.LinkKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -52,15 +44,19 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Optional;
-import com.google.common.util.concurrent.CheckedFuture;
-
-import ch.qos.logback.classic.spi.LoggingEvent;
-import ch.qos.logback.core.Appender;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({LogicalDatastoreType.class, EvcUtils.class})
+@PrepareForTest({LogicalDatastoreType.class})
 public class MdsalUtilsTest {
+
+
+    public static final TopologyId UNI_TOPOLOGY_ID = new TopologyId(new Uri("unimgr:uni"));
+
+    public static final TopologyId EVC_TOPOLOGY_ID = new TopologyId(new Uri("unimgr:evc"));
 
     @Rule
     public final ExpectedException exception = ExpectedException.none();
@@ -96,7 +92,7 @@ public class MdsalUtilsTest {
         InstanceIdentifier<Node> genericNode = InstanceIdentifier
                                                    .create(NetworkTopology.class)
                                                    .child(Topology.class,
-                                                           new TopologyKey(UnimgrConstants.UNI_TOPOLOGY_ID))
+                                                           new TopologyKey(UNI_TOPOLOGY_ID))
                                                    .child(Node.class);
         when(dataBroker.newWriteOnlyTransaction()).thenReturn(transaction);
         doNothing().when(transaction).delete(any(LogicalDatastoreType.class),
@@ -141,7 +137,7 @@ public class MdsalUtilsTest {
         InstanceIdentifier<?> nodeIid = InstanceIdentifier
                 .create(NetworkTopology.class)
                 .child(Topology.class,
-                        new TopologyKey(UnimgrConstants.EVC_TOPOLOGY_ID))
+                        new TopologyKey(EVC_TOPOLOGY_ID))
                 .child(Link.class,
                         new LinkKey(linkId));
         ReadOnlyTransaction transaction = mock(ReadOnlyTransaction.class);
@@ -162,7 +158,7 @@ public class MdsalUtilsTest {
         InstanceIdentifier<?> nodeIid = InstanceIdentifier
                                             .create(NetworkTopology.class)
                                             .child(Topology.class,
-                                                    new TopologyKey(UnimgrConstants.UNI_TOPOLOGY_ID))
+                                                    new TopologyKey(UNI_TOPOLOGY_ID))
                                             .child(Node.class,
                                                     new NodeKey(OVSDB_NODE_ID));
         ReadOnlyTransaction transaction = mock(ReadOnlyTransaction.class);
