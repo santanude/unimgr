c256d54c81056c7df65e331a5407915ca418b1e8
Arif Hussain <arif.hussain@xoriant.com>
2019-04-04 10:16:16 +0530

    Revert "Code committing for interface testing"
    This reverts commit 0306b2dab19b15defebdc76fdeac42f0275f6049.

--- impl/src/main/java/org/opendaylight/unimgr/mef/nrp/impl/connectivityservice/DeleteConnectivityAction.java
+++ impl/src/main/java/org/opendaylight/unimgr/mef/nrp/impl/connectivityservice/DeleteConnectivityAction.java
@@ -68,12 +68,12 @@ public class DeleteConnectivityAction implements Callable<RpcResult<DeleteConnec
     @Override
     public RpcResult<DeleteConnectivityServiceOutput> call() {
         serviceId = new Uuid(input.getServiceIdOrName());
-        LOG.info("service ID =  ", input.getServiceIdOrName());
+        LOG.info("service ID =  ", serviceId);
         
         NrpDao nrpDao = new NrpDao(service.getBroker().newReadOnlyTransaction());
 
         ConnectivityService cs =
-                nrpDao.getConnectivityService(input.getServiceIdOrName());
+                nrpDao.getConnectivityService(serviceId);
         if (cs == null) {
             return RpcResultBuilder
                     .<DeleteConnectivityServiceOutput>failed()
