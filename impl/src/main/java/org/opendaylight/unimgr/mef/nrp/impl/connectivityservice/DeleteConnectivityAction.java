ecb301afaa3e26e966dfd83310c7cda7fef54086
Arif Hussain <arif.hussain@xoriant.com>
2019-04-05 17:11:22 +0530

    Code committing for interface testing Bug (V 1.0.0): Interface going down after deletion

--- impl/src/main/java/org/opendaylight/unimgr/mef/nrp/impl/connectivityservice/DeleteConnectivityAction.java
+++ impl/src/main/java/org/opendaylight/unimgr/mef/nrp/impl/connectivityservice/DeleteConnectivityAction.java
@@ -72,7 +72,7 @@ public class DeleteConnectivityAction implements Callable<RpcResult<DeleteConnec
 
         ConnectivityService cs =
                 nrpDao.getConnectivityService(serviceId);
-        LOG.error("ConnectivityService cs = {}", cs.toString());
+        LOG.info("ConnectivityService cs = {}", cs.toString());
         
         if (cs == null) {
             return RpcResultBuilder
